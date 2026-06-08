13af49b5cac9a1beda5dea5fa8f846aeee4794c8
P3dro08885 <nathanpbacelar2009@gmail.com>
2026-06-08 17:19:19 -0300

    Add files via upload

---
+++ AssinaturaJogos/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styler/styler.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Assinatura de Jogos</h1>
+    <p>Bem-vindo à nossa plataforma de assinatura de jogos!!!</p>
+</body>
+</html>
